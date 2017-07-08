e644d1c1c1742d7b7e46ab7207ec50ce150a57b9
LLihao <h_91@me.com>
2017-07-07 22:01:51 -0700

    basic functionality done

---
+++ js/todo-list.js
@@ -0,0 +1,24 @@
+// Click on list item to mark it as completed
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+// Click on delete to delete list item
+$("ul").on("click", "span", function(event) {
+    // Fade out the list item contain the span
+    $(this).parent().fadeOut(300, function() {
+        // Remove list item after fade out
+        $(this).remove();
+    });
+    // Stop event from bubbling up to parent DOMs
+    event.stopPropagation();
+});
+
+// Add new to-do item to list
+$("#input").keypress(function(event) {
+    // If the enter key is pressed
+    if (event.which === 13) {
+        $("ul").append("<li><span>X</span>" + $(this).val() + "</li>");
+        $(this).val("");
+    }
+})
